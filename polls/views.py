ac6ec09b416c6498b7e7a84df3c7c91ab0d3b92d
Justin Kenel <jfkenel@gmail.com>
2015-05-30 13:45:02 -0400

    Added three new views

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 def index(request):
   return HttpResponse('Hello, world. This is polls index')
 
+def detail(request, question_id):
+  return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+  return HttpResponse("You're looking at the results of question %s." % question_id)
+
+def vote(request, question_id):
+  return HttpResponse("You're voting on question %s." % question_id)
