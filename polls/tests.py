a78025f39b8e98b0da822fe36fccd9da6d39e82e
Justin Kenel <jfkenel@gmail.com>
2015-05-30 16:37:37 -0400

    Don't show future Questions for index

--- polls/tests.py
+++ polls/tests.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.core.urlresolvers import reverse
 from django.utils import timezone
 from django.test import TestCase
 
@@ -22,3 +23,27 @@ class QuestionMethodTests(TestCase):
     recent_question=Question(pub_date=time)
     self.assertEqual(recent_question.was_published_recently(),True)
 
+def create_question(question_text, days):
+  time=timezone.now()+datetime.timedelta(days=days)
+  return Question.objects.create(question_text=question_text, pub_date=time)
+
+class QuestionViewTests(TestCase):
+  def test_index_view_with_no_questions(self):
+    response=self.client.get(reverse('polls:index'))
+    self.assertEqual(response.status_code, 200)
+    self.assertContains(response, "No polls are available.")
+    self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+  def test_index_view_with_a_past_question(self):
+    create_question(question_text="Past question.", days=-30)
+    response=self.client.get(reverse('polls:index'))
+    self.assertQuerysetEqual(
+      response.context['latest_question_list'],
+      ['<Question: Past question.>']
+    )
+
+  def test_index_view_with_a_future_question(self):
+    create_question(question_text="Future question.", days=30)
+    response=self.client.get(reverse('polls:index'))
+    self.assertContains(response, "No polls are available.")
+    self.assertQuerysetEqual(response.context['latest_question_list'],[])
