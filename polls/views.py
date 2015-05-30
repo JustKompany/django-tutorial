0c65b6e851f16b9e74d65b39ca56c9cd73a2a5e7
Justin Kenel <jfkenel@gmail.com>
2015-05-30 15:38:12 -0400

    Add voting for questions

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
 from .models import Question
 
 def index(request):
@@ -17,4 +19,15 @@ def results(request, question_id):
   return HttpResponse("You're looking at the results of question %s." % question_id)
 
 def vote(request, question_id):
-  return HttpResponse("You're voting on question %s." % question_id)
+  p = get_object_or_404(Question, pk=question_id)
+  try:
+    selected_choice=p.choice_set.get(pk=request.POST['choice'])
+  except (KeyError, Choice.DoesNotExist):
+    return render(request, 'polls/detail.html', {
+      'question': p,
+      'error_message': "You didn't select a choice.",
+    })
+  else:
+    selected_choice.votes+=1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
